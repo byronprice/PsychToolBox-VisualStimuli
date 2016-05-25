e58356d1fe56f99c84726679dfd2e20006421832
Byron Price <byron.h.price@gmail.com>
2016-05-25 14:53:11 -0400

    Add file output for analysis

--- Retinotopy.m
+++ Retinotopy.m
@@ -1,23 +1,34 @@
-function [] = Retinotopy(Dist_To_Screen)
+function [] = Retinotopy(AnimalName,DistToScreen,degreeRadius)
 %Retinotopy.m
 %  Display a series of flashing circles to simply determine retinotopy
 %  Each circle will occupy a 1.5 degree radius of visual space
-% INPUT: Dist_To_Screen - physical distance of observer from the screen, in
+% INPUT: DistToScreen - physical distance of observer from the screen, in
 %           units of cm
+%        AnimalName - animal's unique identifier as a number, e.g. 45602
+%        degreeRadius - 
 %
 % Created: 2016/05/24 at 24 Cummington, Boston
 %  Byron Price
-% Updated: 2016/05/24
+% Updated: 2016/05/25
 %  By: Byron Price
 
 directory = pwd;
+
+if nargin < 2
+    DistToScreen = 20;
+    degreeRadius = 2;
+end
+
+Date = datetime('today','Format','yyyy-MM-dd');
+Date = char(Date); Date = strrep(Date,'-','');
 % Acquire a handle to OpenGL, so we can use OpenGL commands in our code:
 global GL;
 
 % Make sure this is running on OpenGL Psychtoolbox:
 AssertOpenGL;
 
-%usb = usb1208FSPlusClass
+usb = usb1208FSPlusClass
+WaitSecs(2);
 
 % Choose screen with maximum id - the secondary display:
 screenid = max(Screen('Screens'));
@@ -32,11 +43,6 @@ Screen('ColorRange', win, 1);
 % Query window size in pixels
 [w_pixels, h_pixels] = Screen('WindowSize', win);
 
-if nargin == 0
-    Dist_To_Screen = 20;
-    degreeRadius = 1.5;
-end
-
 % Retrieve monitor refresh duration
 ifi = Screen('GetFlipInterval', win);
 
@@ -45,11 +51,11 @@ ifi = Screen('GetFlipInterval', win);
 conv_factor = (w_mm/w_pixels+h_mm/h_pixels)/2;
 
 % perform unit conversions
-Radius = ((tan(degreeRadius*(2*pi)/360))*(Dist_To_Screen*10))./conv_factor; % get number of pixels
+Radius = ((tan(degreeRadius*(2*pi)/360))*(DistToScreen*10))./conv_factor; % get number of pixels
      % that 1 degree of visual space will occupy
 Radius = round(Radius);
-CenterX = Radius:Radius*2:w_pixels;
-CenterY = Radius:Radius*2:h_pixels;
+CenterX = w_pixels/2-400:Radius*2:w_pixels/2+400;
+CenterY = h_pixels/2-400:Radius*2:h_pixels/2+400;
 
 dgshader = [directory '/Retinotopy.vert.txt'];
 GratingShader = LoadGLSLProgramFromFiles({ dgshader, [directory '/Retinotopy.frag.txt'] }, 1);
@@ -62,18 +68,23 @@ gratingTex = Screen('SetOpenGLTexture', win, [], 0, GL.TEXTURE_3D,w_pixels,...
 % size
 Color = [0,0,0,0;1,1,1,1];
 
-usb.startRecording;
 
 % Perform initial flip to gray background and sync us to the retrace:
 vbl = Screen('Flip', win);
 ts = vbl;
 
+stimulusLocations = [CenterX',CenterY'];
+fileName = strcat('Retino',Date,'_',num2str(AnimalName),'.mat');fileName
+save(fileName,'stimulusLocations','Radius')
+
+usb.startRecording;
+WaitSecs(5);
 % Animation loop
 for ii=1:length(CenterX)
   for jj=1:length(CenterY)
 
     % Draw the procedural texture as any other texture via 'DrawTexture'
-    %usb.strobe;
+    usb.strobe;
     for kk=1:8
         value = mod(kk,2)+1;
         Screen('DrawTexture', win,gratingTex, [],[],...
@@ -82,10 +93,12 @@ for ii=1:length(CenterX)
             Radius,CenterX(ii),CenterY(jj),0]);
             % Request stimulus onset
             vbl = Screen('Flip', win, vbl + 5*ifi/2);
-
-     end
-     %usb.strobe;
-
+            
+            %usb.triggerON(1,7);
+            %usb.triggerOFF(1,7);
+    end
+    usb.strobe;
+    
      Screen('DrawTexture', win,gratingTex, [],[],...
          [],[],[],[0.5 0.5 0.5 0.5],...
          [], [],[0.5,0.5,0.5,0.5,...
@@ -95,8 +108,10 @@ for ii=1:length(CenterX)
     vbl = vbl+1.0;
   end
 end
+WaitSecs(5);
 usb.stopRecording;
 
 % Close window
 Screen('CloseAll');
+
 end
